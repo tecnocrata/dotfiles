2bad7f6498b6275a54a58d97d69de683a8a7b66e
Enrique Ortuno <enrique@ortuno.net>
2022-10-23 10:26:51 -0400

    git alias added

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -17,8 +17,10 @@ alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gps="git push"
+alias gpso="git push origin"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
+alias gplo="git pull origin"
 alias gb="git branch"
 alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
